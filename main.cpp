cfe714af35057188422ba380c9136f7dfed017d8
GonzaloLeyva <gonzalonleyva@gmail.com>
2024-09-08 21:41:13 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+
+ main()
+{
+    /*Comentario*/
+    printf("hola mundo");
+    printf("\nHola Mundo"); /*Salto de linea*/
+    printf("\nX\nY");
+    printf("\n\nHello World\a");
+    printf("\n\n\n \"\Hola Mundo?\"");
+}
