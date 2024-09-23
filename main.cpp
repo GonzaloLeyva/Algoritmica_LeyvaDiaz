95a368934aa91a06c85ee4cbf750f94f0d1ec840
GonzaloLeyva <gonzalonleyva@gmail.com>
2024-09-23 13:48:24 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,19 +1,14 @@
-/*Programa: Edad*/
+/*Programa: Conversion de Grados*/
+#include <iostream>
+
 using namespace std;
-#include<iostream>
+
 int main()
 {
-    int dia,mes,ano,edad, fecha;
-    cout <<"Ingrese su Dia de Nacimiento: ";
-      cin >>dia;
-   cout<<"Ingrese su mes de Nacimiento: ";
-      cin>>mes;
-   cout<<"Ingrese su ano de Nacimiento: ";
-      cin>>ano;
-   cout<<"Ingrese el ano actual: ";
-      cin>>fecha;
-    edad= fecha-ano;
-   cout<<"su edad es: "<<edad<<" anos "<<endl;
-
+    float celsius, fahrenheit;
+    cout << "Grados Celsius: ";
+    cin >> celsius;
+    fahrenheit=(celsius*9)+32;
+    cout << celsius << "°C son: " << fahrenheit << "°F" <<endl;
     return 0;
 }
