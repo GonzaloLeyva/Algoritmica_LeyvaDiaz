a40ebd187ebe84e5a2e830fcc718f4f392f60a12
GonzaloLeyva <gonzalonleyva@gmail.com>
2024-09-23 14:01:23 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+      char nombre[20],apellido[20];
+    cout <<"Ingrese su nombre: ";
+      cin >>nombre;
+        cout <<"Ingrese su apellido: ";
+      cin >>apellido;
+    cout<<"su nombre es: "<<nombre<<apellido<<endl;
+
+return 0;
+
+
+}
